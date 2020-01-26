62e05bf6409651cc542740292a3f71a413c78d21
Sandra Larsson <sandral@mediagymnasiet.se>
2020-01-26 20:49:08 +0100

    changed css

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 const gallery = document.querySelector('.gallery');
 const proxy = 'https://cors-anywhere.herokuapp.com/';
 
-const query = 'trump';
-const per_page = 20;
+const query = 'games of thrones';
+const per_page = 50;
 
 const url = `${proxy}//api.giphy.com/v1/gifs/search?api_key=NrCOaFKwV1pChh3acIc1oVGxJVPODKt5&q=${query}&limit=${per_page}&offset=0&rating=G&lang=en`;
 
